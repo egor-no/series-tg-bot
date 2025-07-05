960de7ea9850db2ee355a2ddf649f1fa4de7d5c6
egor-no <e.nosko93@ya.ru>
2025-07-05 14:28:19 +0500

    Initial bot

---
+++ src/main/java/SeriesProgressBot.java
@@ -0,0 +1,35 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class SeriesProgressBot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "SeriesProgressBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("BOT_TOKEN");
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String text = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage response = new SendMessage();
+            response.setChatId(String.valueOf(chatId));
+            response.setText("Ты написал: " + text);
+
+            try {
+                execute(response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
